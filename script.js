afbb1b25bd412cc700db05fb641257794ed34782
Novanoory <noorulainrasheed2082@gmail.com>
2024-09-28 00:21:07 +0500

    ok

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function addTask() {
     if (!taskInput.value.trim()) {
         return alert("Todo can't be empty!")
     }
+ // Create a new list item
+ var li = document.createElement('li');
+ li.textContent = taskInput.value;
+
+ // Create a remove button
+ var removeButton = document.createElement('button');
+ removeButton.textContent = 'Remove';
+ removeButton.className = 'remove-btn';
+ removeButton.onclick = function () {
+     taskList.removeChild(li);
+ };
